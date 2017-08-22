b2f003f62c603c79bce2c80813be3bea7aa279d3
Lance Ewing <lance.ewing@gmail.com>
2017-08-22 22:27:59 +0100

    Incremental commit. Move to click is working.

---
+++ ego.js
@@ -0,0 +1,93 @@
+/**
+ * Creates a new Ego. This is the main character whom the player controls. The
+ * name originates with the old Sierra On-Line 3D animated adventure games. There
+ * should be only one instance of this class.
+ * 
+ * @constructor
+ * @extends Actor
+ * @property {number} power The amount of golden power that Ego currently has.
+ * @property {number} rocks The number of rocks that Ego is currently carrying.
+ */
+$.Ego = function() {
+  $.Actor.call(this, 50, 'rgb(197,179,88)', 0.95, 5);
+  this.sprite.classList.add('ego');
+  this.setDirection($.Sprite.OUT);
+  this.power = 0;
+  this.rocks = 0;
+};
+
+$.Ego.prototype = Object.create($.Actor.prototype);
+$.Ego.prototype.constructor = $.Ego;
+
+/**
+ * Updates Ego for the current frame. The update method of every Sprite is 
+ * invoked once per frame. For Ego, it checks the direction keys to see if
+ * Ego's direction and movement needs to change. It also handles jumping,
+ * flying, dropping Rocks, firing glow Bombs, and absorbing energy from the
+ * Orb. It is where most of the player's control of the game takes place.
+ */
+$.Ego.prototype.update = function() {
+  // Mask out left/right/in/out but retain the current jumping directions.
+  var direction = (this.direction & $.Sprite.UP_DOWN);
+  
+  // TODO: Update the direction
+  
+  if ($.Game.mouseButton) {
+    $.Game.mouseButton = 0;
+    this.destZ = $.Game.yMouse * 2;
+    this.destX = $.Game.xMouse;
+  }
+  
+  if ((this.destX != -1) && (this.destZ != -1)) {
+	if (this.touching({cx: this.destX, cy: this.cy, z: this.destZ, radius: -this.radius}, 20)) {
+	  if (this.step > 1) {
+		this.reset();
+	  } else {
+	    this.destX = this.destZ = -1;
+	    this.heading = null;
+	  }
+    } else {
+      this.heading = Math.atan2(this.destZ - $.ego.z, this.destX - $.ego.cx);
+    }
+  }
+  
+  // Update Ego's direction to what was calculated above. The move method will use this 
+  // when moving Ego. The direction is converted into a heading within setDirection.
+  this.setDirection(direction);
+  
+  // Move Ego based on it's direction and heading.
+  this.move();
+
+  // The hit method sets the bounce flag, and it is cleared here.
+  this.bounce = false;
+};
+
+/**
+ * Invoked when Ego has hit another Sprite.
+ * 
+ * @param obj The Sprite that Ego has hit.
+ */
+$.Ego.prototype.hit = function(obj) {
+  // This flag is set so that the update method knows in an efficient way that 
+  // Ego is currently touching something. 
+  this.bounce = true;
+  
+  // Reset the position to the last one that isn't touching another Sprite. Resetting
+  // the position prevents Ego from walking through obstacles. 
+  for (;this.reset() && this.touching(obj););
+};
+
+/**
+ * Invoked by the move method when Ego has hit an edge. If the edge is the ground, then the
+ * edge parameter is undefined. If the edge is one of the four directions, then Ego is 
+ * attempting to exit the current room. 
+ *  
+ * @param {Array} edge If defined, it will be an array that represents the edge that was hit.
+ */
+$.Ego.prototype.hitEdge = function(edge) {
+  if (edge) {
+    // Ego is attempting to exit the room. Check if this is okay and enter the new room 
+    // if it is okay.
+    $.Game.hitEdge(this, edge);
+  }
+};
--- game.js
+++ game.js
@@ -3,6 +3,20 @@
  * core functions of the game that don't relate directly to an individual Sprite.
  */
 $.Game = {
+
+  xMouse: 0,
+  yMouse: 0,
+  mouseButton: 0,	
+
+  /**
+   * The time of the last animation frame. 
+   */ 
+  lastTime: 0,
+  
+  /**
+   * The time difference between the last animation frame and the current animaton frame.
+   */  
+  delta: 0,
   
   items: [  
     ],
@@ -16,7 +30,9 @@ $.Game = {
    */
   start: function() {
     // Get a reference to each of the elements in the DOM that we'll need to update.
+	$.wrap = document.getElementById('wrap');
     $.screen = document.getElementById('screen');
+    $.shadow = document.getElementById('shadow');
     $.wall = document.getElementById('wall');
     $.bricks = document.getElementById('bricks');
     $.items = document.getElementById('itemlist');
@@ -40,6 +56,146 @@ $.Game = {
     // Register click event listeners for the item list.
     
     
+    // Set up the keyboard & mouse event handlers (size reduced way)
+    $.screen.onmousedown = function(e) {
+      $.Game.mouseButton = 1;
+      e.preventDefault();
+    };
+    $.screen.onmouseup = function(e) {
+      $.Game.mouseButton = 0;
+      e.preventDefault();
+    };
+    $.screen.onmousemove = function(e) {
+      $.Game.xMouse = e.pageX - $.wrap.offsetLeft;
+      $.Game.yMouse = e.pageY - $.wrap.offsetTop;
+    };
+    
+    // Initialise and then start the game loop.
+    $.Game.init();
+    $.Game.loop();
+  },
+  
+  /**
+   * Initialised the parts of the game that need initialising on both
+   * the initial start and then subsequent restarts. 
+   */
+  init: function() {
+    // For restarts, we'll need to remove the objects from the screen.
+    if (this.objs) {
+      for (var i=0; i<this.objs.length; i++) {
+        this.objs[i].remove();
+      }
+    }
+    
+    // Set the room back to the start, and clear the object map.
+    this.objs = [];
+    this.room = [0, 0];
+    
+    // Create Ego (the main character) and add it to the screen.
+    $.ego = new $.Ego();
+    $.ego.add();
+    $.ego.setPosition(550, 0, 600);
+    
+    // Enter the starting room.
+    this.newRoom();
+  },
+  
+  /**
+   * This is a wrapper around the main game loop whose primary purpose is to make
+   * the this reference point to the Game object within the main game loop. This 
+   * is the method invoked by requestAnimationFrame and it quickly delegates to 
+   * the main game loop.
+   *  
+   * @param {number} now Time in milliseconds.
+   */
+  _loop: function(now) {
+    $.Game.loop(now);
+  },
+  
+  /**
+   * This is the main game loop, in theory executed on every animation frame.
+   * 
+   * @param {number} now Time. The delta of this value is used to calculate the movements of Sprites.
+   */
+  loop: function(now) {
+    // Immediately request another invocation on the next
+    requestAnimationFrame(this._loop);
+    
+    // Calculates the time since the last invocation of the game loop.
+    this.updateDelta(now);
+    
+    // Game has focus and is not paused, so execute normal game loop, which is
+    // to update all objects on the screen.
+    this.updateObjects();
+    
+    // If after updating all objects, the room that Ego says it is in is different
+    // that what it was previously in, then we trigger entry in to the new room.
+    if (($.ego.room[0] != this.room[0]) || ($.ego.room[1] != this.room[1])) {
+      this.newRoom();
+    }
+  },
+  
+  /**
+   * Updates the delta, which is the difference between the last time and now. Both values
+   * are provided by the requestAnimationFrame call to the game loop. The last time is the
+   * value from the previous frame, and now is the value for the current frame. The difference
+   * between them is the delta, which is the time between the two frames. From this value
+   * it can calculate the stepFactor, which is used in the calculation of the Sprites' motion.
+   * In this way if a frame is skipped for some reason, the Sprite position will be updated to 
+   * compensate.
+   * 
+   * @param {Object} now The current time provided in the invocation of the game loop.
+   */
+  updateDelta: function(now) {
+    this.delta = now - (this.lastTime? this.lastTime : (now - 16));
+    this.stepFactor = this.delta * 0.06;
+    this.lastTime = now;
+  },
+  
+  /**
+   * The main method invoked on every animation frame when the game is unpaused. It 
+   * interates through all of the Sprites and invokes their update method. The update
+   * method will invoke the move method if the calculated position has changed. This
+   * method then tests if the Sprite is touching another Sprite. If it is, it invokes
+   * the hit method on both Sprites. 
+   */
+  updateObjects: function() {
+    var i=-1, j, a1=$.ego, a2;
+    var objsLen = this.objs.length;
+
+    // Iterate over all of the Sprites in the current room, invoking update on each on.
+    for (;;) {
+      if (a1) {
+        a1.update();
+
+        // Check if the Sprite is touching another Sprite.
+        for (j = i + 1; j < objsLen; j++) {
+          a2 = this.objs[j];
+          if (a2 && a1.touching(a2)) {
+            // If it is touching, then invoke hit on both Sprites. They might take 
+            // different actions in response to the hit.
+            a1.hit(a2);
+            a2.hit(a1);
+          }
+        }
+        
+        // Clears the Sprite's moved flag, which is only of use to the hit method.
+        a1.moved = false;
+      }
+      
+      if (++i < objsLen) {
+        a1 = this.objs[i];
+      } else {
+        break;
+      }
+    }
+  },
+  
+  /**
+   * Invoked when Ego is entering a room.  
+   */
+  newRoom: function() {
+
   },
   
   scrollInv: function(dir) {
